8affe3d8d21d6cdd9d7bf4c1892fe2bd4fb97749
Bankole Olaniyi Sunday <adeniyisunday2244@gmail.com>
2025-11-03 19:38:08 +0100

    change file path for deployment

--- src/app/[dashboard]/user/page.tsx
+++ src/app/[dashboard]/user/page.tsx
@@ -1,10 +1,10 @@
-import Dashboard from '@/Component/dashboard/dashboard_1'
-import React from 'react'
+// import Dashboard from '@/component/dashboard/dashboard_1'
+// import React from 'react'
 
-export default function User_Dashboard() {
-  return (
-    <div>
-      <Dashboard/>
-    </div>
-  )
-}
+// export default function User_Dashboard() {
+//   return (
+//     <div>
+//       <Dashboard/>
+//     </div>
+//   )
+// }
