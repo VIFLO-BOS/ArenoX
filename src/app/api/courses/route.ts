7382643973281fe540fef75c9578839dbff31f42
Bankole Olaniyi Sunday <adeniyisunday2244@gmail.com>
2025-12-16 16:37:13 +0100

    RBAC Configuration complete eslint error 2 solved

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -7,7 +7,6 @@ export async function POST(req: NextRequest) {
     const courseCollection = db.collection("courses");
 
     const formData = await req.formData();
-
     const courseData = Object.fromEntries(formData);
 
     if (
@@ -16,35 +15,37 @@ export async function POST(req: NextRequest) {
       courseData.title.length < 10 ||
       courseData.title.length > 100
     ) {
-      return new Response(JSON.stringify({ message: "Invalid title" }), {
-        status: 400,
-      });
+      return NextResponse.json({ message: "Invalid title" }, { status: 400 });
     }
-
-   
 
     const existing = await courseCollection.findOne({
       title: courseData.title,
     });
     if (existing) {
       return NextResponse.json(
-        { message: "Course title already exit" },
+        { message: "Course title already exists" },
         { status: 400 }
       );
     }
 
     const result = await courseCollection.insertOne(courseData);
 
-    if (result) {
+    if (result.acknowledged) {
       return NextResponse.json(
         { message: "Course submitted successfully", data: courseData },
-        { status: 201 } // 201 Created status
+        { status: 201 }
       );
     }
+
+    return NextResponse.json(
+      { message: "Failed to save course" },
+      { status: 500 }
+    );
   } catch (error) {
     console.error("Error in POST /api/courses:", error);
-    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
-      status: 500,
-    });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
