fb400a73b8ef7cacb590bc3091e5c59761bf68ac
Bankole Olaniyi <adeniyisunday2244@gmail.com>
2026-01-05 19:51:17 +0100

    auth client error fixed

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -1,12 +1,12 @@
 "use client";
 
 import { createAuthClient } from "better-auth/react";
-import type { auth } from "./auth";
+import type { getAuth } from "./auth";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 // @ auth-client-config : create and configure authentication client with base URL and plugins
 export const authClient = createAuthClient({
-  plugins: [inferAdditionalFields<typeof auth>()],
+  plugins: [inferAdditionalFields<typeof getAuth>()],
 });
 
 // @ exports : export authentication methods for use in client components
