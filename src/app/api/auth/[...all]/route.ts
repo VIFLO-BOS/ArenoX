6e1c4070d7db3dd194db91d190acb34dc02785fd
Bankole Olaniyi <adeniyisunday2244@gmail.com>
2026-01-05 19:54:28 +0100

    all route error ficd

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,5 @@
-import { auth } from "@/app/lib/auth";
+import { getAuth} from "@/app/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 // @ auth-handlers : export POST and GET handlers for better-auth authentication routes
-export const { POST, GET } = toNextJsHandler(auth);
+export const { POST, GET } = toNextJsHandler(await getAuth());
